74047bdfc09d4c4c06a6f1fad5f7e546bc3098f7
Ju5t1n1005 <154566237+Ju5t1n1005@users.noreply.github.com>
2024-01-12 05:45:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# main.py
 
+# Import functions from the Task A and Task B modules
+# from A.Task_A import run_task_A
+# from B.Task_B import run_task_B  
+
+if __name__ == "__main__":
+    # Uncomment the line below to run Task A
+    # run_task_A()
+
+    # Uncomment the line below to run Task B
+    # run_task_B()  
